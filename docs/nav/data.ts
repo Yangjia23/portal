4b93af735352b9be8c728b7116f0f694985b3b0b
yangj <yangj@cloudglab.com>
2024-01-03 09:51:32 +0800

    feat: add new links

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -404,19 +404,13 @@ export const NAV_DATA: NavData[] = [
     ]
   },
   {
-    title: '前端学习资料',
+    title: '资料网站',
     items: [
       {
-        icon: 'https://developer.mozilla.org/apple-touch-icon.6803c6f0.png',
-        title: 'MDN | Web 开发者指南',
-        desc: 'Mozilla 的开发者平台，提供了大量关于 HTML、CSS 和 JavaScript 的详细文档以及广泛的 Web API 参考资',
-        link: 'https://developer.mozilla.org/zh-CN'
-      },
-      {
-        icon: 'https://static.runoob.com/images/favicon.ico',
-        title: '菜鸟教程',
-        desc: '学的不仅是技术，更是梦想！',
-        link: 'https://www.runoob.com'
+        icon: 'https://zh.z-library.se/apple-touch-icon.png?v=1',
+        title: 'z-library',
+        desc: '号称世界最大的免费电子图书馆',
+        link: 'https://zh.z-library.se/'
       },
       {
         icon: '/icons/es6.svg',
